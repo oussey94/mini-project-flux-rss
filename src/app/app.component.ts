4bb7879c7fe6c885c16376d1bf0c1acdb3b6a1d9
oussey94 <mbodjiousseynou94@gmail.com>
2022-04-21 12:10:53 +0000

    after update with rss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
+import { NewsRss } from './model/new-Rss';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import * as xml2js from "xml2js";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mini-project-flux-rss';
+  RssData?: NewsRss;
+  constructor(private http: HttpClient) {}
+  GetRssFeedData() {
+    const requestOptions: Object = {
+      observe: "body",
+      responseType: "text"
+    };
+    this.http
+      .get<any>("https://www.lemonde.fr/rss/en_continu.xml", requestOptions)
+      .subscribe(data => {
+        let parseString = xml2js.parseString;
+        parseString(data, (err, result: NewsRss) => {
+          this.RssData = result;
+          console.log("Rssss:", this.RssData);
+        });
+      });
+  }
 }
